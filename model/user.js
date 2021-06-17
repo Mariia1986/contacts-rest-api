6b5341231cbeec1c6ab13c62a508865ef6e4a479
Mariia1986 <renij1986@gmail.com>
2021-06-17 16:09:20 +0300

    дбавлены ключи

--- model/user.js
+++ model/user.js
@@ -30,6 +30,16 @@ const userSchema = new Schema(
           type: String,
           default: null,
         },
+        {
+          verify: {
+            type: Boolean,
+            default: false,
+          },
+          verifyToken: {
+            type: String,
+            required: [true, 'Verify token is required'],
+          },
+        }
 
        
       }
